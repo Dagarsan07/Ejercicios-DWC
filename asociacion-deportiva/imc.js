d63124c0d1d3f257a65882629fc18acf8c47d349
DAVID1STMACHINE\David <garciasanchezdavid097@gmail.com>
2023-09-17 19:51:03 +0200

    Cifrado César terminado. Asociación deportiva en progreso

---
+++ asociacion-deportiva/imc.js
@@ -0,0 +1,11 @@
+const tablaIMC = [
+    
+]
+
+function calcularIMC() {
+    const altura = document.getElementById("altura").value;
+    const peso = document.getElementById("peso").value;
+    let imc = peso / ((altura / 100) ** 2);
+
+    
+}
--- cifrado-cesar/cifrado-cesar.js
+++ cifrado-cesar/cifrado-cesar.js
@@ -1,22 +1,41 @@
-const numDesplazamiento = 6;
-
-function cifrarMensaje() {
-    let abecedario = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
+const abecedario = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
+const indiceDesplazamiento = 6;
+function cifrar() {
     let mensaje = document.getElementById("mensaje").value.toUpperCase();
-    let mensajeCifrado = [];
-    let letraCifrado;
+    let mensajeCifrado = "";
     for (i = 0; i < mensaje.length; i++) {
-        let letra = mensaje.substring[i];
-        for (i = 0; i < abecedario.length; i++) {
-            if (letra == abecedario[i]) {
-                if ((abecedario.length - i) < numDesplazamiento) {
-                    letraCifrado = abecedario[numDesplazamiento - (abecedario.length - i)];
-                } else {
-                    letraCifrado = abecedario[i + numDesplazamiento];
-                }
-                mensajeCifrado.concat(letraCifrado);
+        let letra = mensaje.charAt(i);
+        let pos = abecedario.indexOf(letra);
+
+        if(pos == -1) {
+            mensajeCifrado += letra;
+        } else {
+            if ((pos + indiceDesplazamiento) > abecedario.length - 1) {
+                mensajeCifrado += abecedario[indiceDesplazamiento - (abecedario.length - pos)];
+            } else {
+                mensajeCifrado += abecedario[pos + indiceDesplazamiento];
             }
         }
     }
-    alert(mensajeCifrado.toString()); 
+    document.getElementById("mensaje").value = mensajeCifrado;
+}
+
+function descifrar() {
+    let mensaje = document.getElementById("mensaje").value.toUpperCase();
+    let mensajeDescifrado = "";
+    for (i = 0; i < mensaje.length; i++) {
+        let letra = mensaje.charAt(i);
+        let pos = abecedario.indexOf(letra);
+
+        if(pos == -1) {
+            mensajeDescifrado += letra;
+        } else {
+            if ((pos - indiceDesplazamiento) < 0) {
+                mensajeDescifrado += abecedario[abecedario.length + (pos - indiceDesplazamiento)];
+            } else {
+                mensajeDescifrado += abecedario[pos - indiceDesplazamiento];
+            }
+        }
+    }
+    document.getElementById("mensaje").value = mensajeDescifrado;
 }
