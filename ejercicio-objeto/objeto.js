1b6d812852f6edc6d0e914f2c06b1c840aecec9d
David García Sánchez <dgarcia@hiberus.com>
2023-09-26 18:28:59 +0200

    Ejercicio objeto: Parte con class hecha (falta arreglar "generalogin()")

---
+++ ejercicio-objeto/objeto.js
@@ -0,0 +1,9 @@
+function Usuario(nombre, apellidos, dni, nacimiento, provincia) {
+    this._nombre = nombre;
+    this._apellidos = apellidos;
+    this._dni = dni;
+    this._nacimiento = nacimiento;
+    this._provincia = provincia;
+
+    
+}
--- ejercicio-objeto/usuario.js
+++ ejercicio-objeto/usuario.js
@@ -1,16 +1,16 @@
-export class Usuario {
+class Usuario {
     #_nombre;
     #_apellidos;
     #_dni;
     #_nacimiento;
     #_provincia;
 
-    constructor() {
-        this.#_nombre;
-        this.#_apellidos;
-        this.#_dni;
-        this.#_nacimiento;
-        this.#_provincia;
+    constructor(nombre = "David", apellidos = "García Sánchez", dni = "14983420T", nacimiento = 2004, provincia = "Vizcaya") {
+        this.#_nombre = nombre;
+        this.#_apellidos = apellidos;
+        this.#_dni = dni;
+        this.#_nacimiento = nacimiento;
+        this.#_provincia = provincia;
     }
 
     get nombre() {
@@ -55,7 +55,7 @@ export class Usuario {
 
     generaLogin() {
         const inicialNom = this.#_nombre.charAt(0);
-        const arrApellidos = this.#_apellidos.split(", ");
+        const arrApellidos = this.#_apellidos.split(" ");
         const inicialAp1 = arrApellidos[0].charAt(0);
         const inicialAp2 = arrApellidos[1].charAt(0);
         const annoDosDigitos = this.#_nacimiento.toString().substring(2);
@@ -68,10 +68,38 @@ export class Usuario {
     }
 
     toString() {
-        console.log(this.#_nombre + this.#_apellidos + this.#_dni + this.#_nacimiento + this.#_provincia);
+        return this.#_nombre + this.#_apellidos + this.#_dni + this.#_nacimiento + this.#_provincia;
     }
 
     toHTML() {
-        
+        return `
+            <ul>
+                <li>Nombre: ${this.#_nombre}</li>
+                <li>Apellidos: ${this.#_apellidos}</li>
+                <li>DNI: ${this.#_dni}</li>
+                <li>Fecha de nacimiento: ${this.#_nacimiento}</li>
+                <li>Provincia: ${this.#_provincia}</li>
+            </ul>
+        `;
     }
+}
+
+const usuario1 = new Usuario();
+const usuario2 = new Usuario("Elena", "López Torres", "12345678A", 2012, "Gipuzkoa");
+const usuario3 = new Usuario("Adrián", "Rubio Castañares", "87654321B", 2006, "Lleida");
+const usuario4 = new Usuario("Hugo", "Amor González", "43218765A", 1995, "Algecirás");
+
+const arrayUsuarios = [usuario1, usuario2, usuario3, usuario4];
+
+function cargarDatos() {
+    arrayUsuarios.forEach(usuario => {
+        console.log("Nombre: " + usuario.nombre);
+        console.log("Apellidos: " + usuario.apellidos);
+        console.log("DNI: " + usuario.dni);
+        console.log("Fecha de nacimiento: " + usuario.nacimiento);
+        console.log("Provincia: " + usuario.provincia);
+    
+        document.querySelector('#propiedades').innerHTML += usuario.toHTML();
+        document.querySelector('#usuarios').innerHTML += usuario.generaLogin();
+    });
 }
