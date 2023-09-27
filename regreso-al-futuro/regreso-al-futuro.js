078ff9f0ced3938790ac89f5d7bd85dc2ee3ddb1
David García Sánchez <dgarcia@hiberus.com>
2023-09-27 18:10:47 +0200

    Validación de campos vacíos, años bisiestos, nº de dia > 28 o 29 en febrero y < 31 en meses que tienen 30 días implementado. Error al comprobar si el la hora y los minutos de las dos fechas son los mismos

--- regreso-al-futuro/regreso-al-futuro.js
+++ regreso-al-futuro/regreso-al-futuro.js
@@ -1,31 +1,101 @@
 const meses = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
 
-function viajeTemporal() {
+function comprobarFechas() {
     let tiempoViajado = document.querySelector('#viajeResult');
-    tiempoViajado.innerHTML = '';
-    const diaActual = document.querySelector('#ogDay').valueAsNumber;
-    let mesActual = document.querySelector('#ogMonth').value.toLowerCase();
-    let a = 0;
-    do {
-        mesActual = mesActual == meses[a] ? a : mesActual;
-        a++;
-    } while (isNaN(mesActual) && a < meses.length);
     const annoActual = document.querySelector('#ogYear').valueAsNumber;
+    let mesActual = document.querySelector('#ogMonth').value.toLowerCase();
+    let diaActual = document.querySelector('#ogDay').valueAsNumber;
     const horaActual = document.querySelector('#ogHour').valueAsNumber;
     const minActual = document.querySelector('#ogMin').valueAsNumber;
-    const fechaActual = new Date(annoActual, mesActual, diaActual, horaActual, minActual);
-    
-    const diaDestino = document.querySelector('#desDay').valueAsNumber;
-    let mesDestino = document.querySelector('#desMonth').value.toLowerCase();
-    let d = 0;
-    do {
-        mesDestino = mesDestino == meses[d] ? d : mesDestino;
-        d++;
-    } while (isNaN(mesDestino) && d < meses.length);
+
     const annoDestino = document.querySelector('#desYear').valueAsNumber;
+    let mesDestino = document.querySelector('#desMonth').value.toLowerCase();
+    let diaDestino = document.querySelector('#desDay').valueAsNumber;
     const horaDestino = document.querySelector('#desHour').valueAsNumber;
     const minDestino = document.querySelector('#desMin').valueAsNumber;
-    const fechaDestino = new Date(annoDestino, mesDestino, diaDestino, horaDestino, minDestino);
+
+    debugger;
+    if (!isNaN(annoActual) || mesActual != "" || !isNaN(diaActual) || !isNaN(horaActual) || !isNaN(minActual) || !isNaN(annoDestino) || mesDestino != "" || !isNaN(diaDestino) || !isNaN(horaDestino) || !isNaN(minDestino)) {
+        if (!meses.includes(mesActual) || !meses.includes(mesDestino)) {
+            tiempoViajado.innerHTML = '<h5 class="px-3 py-3">Flipas chaval. Como va a ser eso un mes?? Anda introduce uno válido ahora mismo</h5>';
+            return;
+        }
+        
+        switch (mesActual) {
+            case "apr":
+            case "jun":
+            case "sep":
+            case "nov":
+                mesActual = meses.indexOf(mesActual) + 1;
+                diaActual = diaActual - 30;
+                break;
+            case "feb":
+                if (annoActual % 4 == 0) {
+                    if (annoActual % 100 != 0 || annoActual % 400 == 0) {
+                        if (diaActual > 29) {
+                            mesActual = meses.indexOf(mesActual) + 1;
+                            diaActual = diaActual - 29;
+                        }
+                    }
+                } else {
+                    mesActual = meses.indexOf(mesActual);
+                }
+                break;
+            case "jan":
+            case "mar":
+            case "may":
+            case "jul":
+            case "aug":
+            case "oct":
+            case "dec":
+                mesActual = meses.indexOf(mesActual);
+                break;
+        }
+    
+        switch (mesDestino) {
+            case "apr":
+            case "jun":
+            case "sep":
+            case "nov":
+                mesDestino = meses.indexOf(mesDestino) + 1;
+                diaDestino = diaDestino - 30;
+                break;
+            case "feb":
+                if (annoActual % 4 == 0) {
+                    if (annoActual % 100 != 0 || annoActual % 400 == 0) {
+                        if (diaDestino > 29) {
+                            mesDestino = meses.indexOf(mesDestino) + 1;
+                            diaDestino = diaDestino - 29;
+                        }
+                    }
+                } else {
+                    mesDestino = meses.indexOf(mesDestino);
+                }
+                break;
+            case "jan":
+            case "mar":
+            case "may":
+            case "jul":
+            case "aug":
+            case "oct":
+            case "dec":
+                mesDestino = meses.indexOf(mesDestino);
+                break;
+        }
+    
+        const fechaActual = new Date(annoActual, mesActual, diaActual, horaActual, minActual);
+        const fechaDestino = new Date(annoDestino, mesDestino, diaDestino, horaDestino, minDestino);
+    
+        viajeTemporal(fechaActual, fechaDestino);
+    } else {
+        tiempoViajado.innerHTML = `<h5 class="px-3 py-3">Pero tu donde andas?? Te faltan campos por rellenar</h5>`;
+        return; 
+    }
+}
+
+function viajeTemporal(fechaActual, fechaDestino) {
+    let tiempoViajado = document.querySelector('#viajeResult');
+    tiempoViajado.innerHTML = '';
 
     let viajeMiliseg = 0;
     let viajeMinutos = 0;
