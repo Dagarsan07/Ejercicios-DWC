463c2f7da69437e7fc8bbf8cc065af7e1edc3216
David García Sánchez <dgarcia@hiberus.com>
2023-09-28 18:39:36 +0200

    Validación de los campos de mes, dia, hora y minutos. Falta mensaje avisando que los caracteres diferentes de los números en los campos numéricos no valen

--- regreso-al-futuro/regreso-al-futuro.js
+++ regreso-al-futuro/regreso-al-futuro.js
@@ -1,23 +1,33 @@
 const meses = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
 
 function comprobarFechas() {
-    let tiempoViajado = document.querySelector('#viajeResult');
+    let mensajeInfo = document.querySelector('#mensaje');
+    let msgDiaInvalido = '<h5 class="px-3 py-3">No puedo creer que no sepas cuanto días tiene cada mes</h5>';
+
     const annoActual = document.querySelector('#ogYear').valueAsNumber;
     let mesActual = document.querySelector('#ogMonth').value.toLowerCase();
     let diaActual = document.querySelector('#ogDay').valueAsNumber;
     const horaActual = document.querySelector('#ogHour').valueAsNumber;
     const minActual = document.querySelector('#ogMin').valueAsNumber;
-
+    
     const annoDestino = document.querySelector('#desYear').valueAsNumber;
     let mesDestino = document.querySelector('#desMonth').value.toLowerCase();
     let diaDestino = document.querySelector('#desDay').valueAsNumber;
     const horaDestino = document.querySelector('#desHour').valueAsNumber;
     const minDestino = document.querySelector('#desMin').valueAsNumber;
-
-    debugger;
-    if (!isNaN(annoActual) || mesActual != "" || !isNaN(diaActual) || !isNaN(horaActual) || !isNaN(minActual) || !isNaN(annoDestino) || mesDestino != "" || !isNaN(diaDestino) || !isNaN(horaDestino) || !isNaN(minDestino)) {
+    
+    if (!isNaN(annoActual) && mesActual != "" && !isNaN(diaActual) && !isNaN(horaActual) && !isNaN(minActual) && !isNaN(annoDestino) && mesDestino != "" && !isNaN(diaDestino) && !isNaN(horaDestino) && !isNaN(minDestino)) {
         if (!meses.includes(mesActual) || !meses.includes(mesDestino)) {
-            tiempoViajado.innerHTML = '<h5 class="px-3 py-3">Flipas chaval. Como va a ser eso un mes?? Anda introduce uno válido ahora mismo</h5>';
+            mensajeInfo.innerHTML = '<h5 class="px-3 py-3">Flipas chaval. Como va a ser eso un mes?? Anda introduce uno válido ahora mismo</h5>';
+            return;
+        } else if (diaActual < 1 || diaActual > 31 || diaDestino < 1 || diaDestino > 31)  {
+            mensajeInfo.innerHTML = msgDiaInvalido;
+            return;
+        } else if (horaActual < 0 || horaActual > 23 || minActual < 0 || minActual > 59) {
+            mensajeInfo.innerHTML = `<h5 class="px-3 py-3">Las ${horaActual}:${minActual}?? Que será lo próximo, las invisibles y media??</h5>`;
+            return;
+        } else if (horaDestino < 0 || horaDestino > 23 || minDestino < 0 || minDestino > 59) {
+            mensajeInfo.innerHTML = `<h5 class="px-3 py-3">Las ${horaDestino}:${minDestino}?? Que será lo próximo, las invisibles y media??</h5>`;
             return;
         }
         
@@ -26,21 +36,38 @@ function comprobarFechas() {
             case "jun":
             case "sep":
             case "nov":
-                mesActual = meses.indexOf(mesActual) + 1;
-                diaActual = diaActual - 30;
-                break;
-            case "feb":
-                if (annoActual % 4 == 0) {
-                    if (annoActual % 100 != 0 || annoActual % 400 == 0) {
-                        if (diaActual > 29) {
-                            mesActual = meses.indexOf(mesActual) + 1;
-                            diaActual = diaActual - 29;
-                        }
-                    }
+                if (diaActual > 30) {
+                    // mesActual = meses.indexOf(mesActual) + 1;
+                    // diaActual = diaActual - 30;
+                    mensajeInfo.innerHTML = msgDiaInvalido;
+                    return;
                 } else {
                     mesActual = meses.indexOf(mesActual);
                 }
                 break;
+            case "feb":
+                if (diaActual > 28) {
+                    if (annoActual % 4 == 0) {
+                        if (annoActual % 100 != 0 || annoActual % 400 == 0) {
+                            if (diaActual == 29) {
+                                // mesActual = meses.indexOf(mesActual) + 1;
+                                // diaActual = diaActual - 29;
+                                // break;
+                                mesActual = meses.indexOf(mesActual);
+                                break;
+                            }
+                            mensajeInfo.innerHTML = msgDiaInvalido;
+                            return;
+                        }
+                        mensajeInfo.innerHTML = msgDiaInvalido;
+                        return;
+                    }
+                    mensajeInfo.innerHTML = msgDiaInvalido;
+                    return;
+                } else {
+                    mesActual = meses.indexOf(mesActual);
+                    break;
+                }
             case "jan":
             case "mar":
             case "may":
@@ -57,21 +84,38 @@ function comprobarFechas() {
             case "jun":
             case "sep":
             case "nov":
-                mesDestino = meses.indexOf(mesDestino) + 1;
-                diaDestino = diaDestino - 30;
-                break;
-            case "feb":
-                if (annoActual % 4 == 0) {
-                    if (annoActual % 100 != 0 || annoActual % 400 == 0) {
-                        if (diaDestino > 29) {
-                            mesDestino = meses.indexOf(mesDestino) + 1;
-                            diaDestino = diaDestino - 29;
-                        }
-                    }
+                if (diaDestino > 30) {
+                    // mesDestino = meses.indexOf(mesDestino) + 1;
+                    // diaDestino = diaDestino - 30;
+                    mensajeInfo.innerHTML = msgDiaInvalido;
+                    return;
                 } else {
                     mesDestino = meses.indexOf(mesDestino);
                 }
                 break;
+            case "feb":
+                if (diaDestino > 28) {
+                    if (annoDestino % 4 == 0) {
+                        if (annoDestino % 100 != 0 || annoDestino % 400 == 0) {
+                            if (diaDestino == 29) {
+                                // mesDestino = meses.indexOf(mesDestino) + 1;
+                                // diaDestino = diaDestino - 29;
+                                // break;
+                                mesDestino = meses.indexOf(mesDestino);
+                                break;
+                            }
+                            mensajeInfo.innerHTML = msgDiaInvalido;
+                            return;
+                        }
+                        mensajeInfo.innerHTML = msgDiaInvalido;
+                        return;
+                    }
+                    mensajeInfo.innerHTML = msgDiaInvalido;
+                    return;
+                } else {
+                    mesDestino = meses.indexOf(mesDestino);
+                    break;
+                }
             case "jan":
             case "mar":
             case "may":
@@ -88,14 +132,14 @@ function comprobarFechas() {
     
         viajeTemporal(fechaActual, fechaDestino);
     } else {
-        tiempoViajado.innerHTML = `<h5 class="px-3 py-3">Pero tu donde andas?? Te faltan campos por rellenar</h5>`;
+        mensajeInfo.innerHTML = `<h5 class="px-3 py-3">Pero tu donde andas?? Te faltan campos por rellenar</h5>`;
         return; 
     }
 }
 
 function viajeTemporal(fechaActual, fechaDestino) {
-    let tiempoViajado = document.querySelector('#viajeResult');
-    tiempoViajado.innerHTML = '';
+    let mensajeInfo = document.querySelector('#mensaje');
+    mensajeInfo.innerHTML = '';
 
     let viajeMiliseg = 0;
     let viajeMinutos = 0;
@@ -107,29 +151,31 @@ function viajeTemporal(fechaActual, fechaDestino) {
         viajeMiliseg = fechaDestino - fechaActual;
         viajeAnnos = viajeMiliseg / (1000 * 60 * 60 * 24 * 365);
         viajeDias = (viajeAnnos - Math.floor(viajeAnnos)) * 365;
-        viajeHoras = (viajeDias - Math.floor(viajeDias)) * 24;
-        viajeMinutos = (viajeHoras - Math.floor(viajeHoras)) * 60;
-        if (Math.round(viajeMinutos) == 60) {
-            viajeMinutos = 0;
-            viajeHoras++;
-        }
-        tiempoViajado.innerHTML = `
-            <h5 class="px-3 py-3">Viajarás al futuro ${Math.floor(viajeAnnos)} años, ${Math.floor(viajeDias)} días, ${Math.floor(viajeHoras)} horas y ${Math.round(viajeMinutos)} minutos</h5>
+        viajeHoras = fechaDestino.getHours() - fechaActual.getHours();
+        if (viajeHoras < 0) {viajeHoras = 24 + viajeHoras};
+        viajeMinutos = fechaDestino.getMinutes() - fechaActual.getMinutes();
+        if (viajeMinutos < 0) {
+            viajeMinutos = 60 + viajeMinutos;
+            viajeHoras--;
+        };
+        mensajeInfo.innerHTML = `
+            <h5 class="px-3 py-3">Viajarás al futuro ${Math.floor(viajeAnnos)} años, ${Math.floor(viajeDias)} días, ${viajeHoras} horas y ${viajeMinutos} minutos</h5>
         `;
     } else if (fechaActual > fechaDestino) {
         viajeMiliseg = fechaActual - fechaDestino;
         viajeAnnos = viajeMiliseg / (1000 * 60 * 60 * 24 * 365);
         viajeDias = (viajeAnnos - Math.floor(viajeAnnos)) * 365;
-        viajeHoras = (viajeDias - Math.floor(viajeDias)) * 24;
-        viajeMinutos = (viajeHoras - Math.floor(viajeHoras)) * 60;
-        if (minActual == minDestino) {viajeMinutos = 0};
-        if (horaActual == horaDestino) {viajeHoras = 0};
-        tiempoViajado.innerHTML = `
-            <h5 class="px-3 py-3">Viajarás al pasado ${Math.floor(viajeAnnos)} años, ${Math.floor(viajeDias)} días, ${Math.round(viajeHoras)} horas y ${Math.round(viajeMinutos)} minutos</h5>
+        viajeHoras = fechaActual.getHours() - fechaDestino.getHours();
+        if (viajeHoras < 0) {viajeHoras = 24 + viajeHoras};
+        viajeMinutos = fechaActual.getMinutes() - fechaDestino.getMinutes();
+        if (viajeMinutos < 0) {
+            viajeMinutos = 60 + viajeMinutos;
+            viajeHoras--;
+        };
+        mensajeInfo.innerHTML = `
+            <h5 class="px-3 py-3">Viajarás al pasado ${Math.floor(viajeAnnos)} años, ${Math.floor(viajeDias)} días, ${viajeHoras} horas y ${viajeMinutos} minutos</h5>
         `;
-    } else if (isNaN(fechaActual)|| isNaN(fechaDestino)){
-        tiempoViajado.innerHTML = '<h5 class="px-3 py-3">Alto ahí master. Pusiste algo mal. Te quedas en tu sitio</h5>';
     } else {
-        tiempoViajado.innerHTML = '<h5 class="px-3 py-3">A no ser que pongas dos fechas diferentes, no voy a hacer nada</h5>';
+        mensajeInfo.innerHTML = '<h5 class="px-3 py-3">A no ser que pongas dos fechas diferentes, no voy a hacer nada</h5>';
     }
 }
